1efe2649ba6bd57f4034ee0249e971741dca249e
Darrian DeVaughnn <devondevaughnn@gmail.com>
2021-05-07 17:23:28 -0700

    finished adding models for table

--- club/models.py
+++ club/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class Meeting(models.Model):
+    title = models.CharField(max_length=255)
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=255)
+    Agenda = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.meetingtitle
+
+    class Meta:
+        db_table = "meeting"
+
+
+class MeetingMinutes(models.Model):
+    title = models.CharField(max_length=255)
+    meetingid = models.ForeignKey(Meeting, on_delete=models.DO_NOTHING)
+    attendance = models.ManyToManyField(User)
+    minutestext = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = "meetingminutes"
+
+
+class Resource(models.Model):
+    name = models.CharField(max_length=255)
+    resourcetype = models.CharField(max_length=255)
+    resourceurl = models.URLField(null=True, blank=True)
+    dateentered = models.DateField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "resource"
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    date = models.DateField()
+    time = models.TimeField()
+    description = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = "event"
